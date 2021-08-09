9059f69c2b2e16b771d1ff9fb79336968d8bb512
Vinayak1337 <vinayak111kumar@gmail.com>
2021-08-09 15:15:06 +0530

    changing status codes

--- src/Routes/Verify.js
+++ src/Routes/Verify.js
@@ -9,11 +9,11 @@ VerifyRouter.get('/', async (req, res) => {
 	if (!(username && email)) return res.status(202).json({ message: 'No body' });
 	if (username) {
 		const user = await users.findOne({ username });
-		if (user) return res.status(408).json({ message: 'Username is not available' });
+		if (user) return res.status(406).json({ message: 'Username is not available' });
 	}
 	else if (email) {
 		const user = await users.findOne({ email });
-		if (user) return res.status(408).json({ message: 'Email is not available' });
+		if (user) return res.status(406).json({ message: 'Email is not available' });
 	}
 	res.status(200).json('Username & Email are available');
 });
